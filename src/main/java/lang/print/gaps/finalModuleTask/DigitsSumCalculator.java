e5e2bd3959f252021d0c9c7a9fd1dc02beb9db95
LoonyJava <loony.java@gmail.com>
2022-10-27 16:52:27 +0500

    FINAL MODULE TASK

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sum = 0;
+        int input_int = number;
+        while (input_int != 0) {
+            sum += (input_int % 10);
+            input_int = input_int / 10;
+        }
+        System.out.println(sum);
     }
 }
