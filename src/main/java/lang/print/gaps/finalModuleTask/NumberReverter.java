9167988b87b0dcb7c74d5b110b46150cb0bdaf4a
LoonyJava <loony.java@gmail.com>
2022-10-27 16:43:55 +0500

    FINAL MODULE TASK

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int reversedNum = 0;
+        int input_int = number;
+        while (input_int != 0) {
+            reversedNum = reversedNum * 10 + input_int % 10;
+            input_int = input_int / 10;
+        }
+        System.out.println(reversedNum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -1,6 +1,9 @@
 package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
-    public void toFahrenheit(int temperatureCelsius){
+    public void toFahrenheit(int temperatureCelsius) {
+        double temperatureFahrenheit =  temperatureCelsius * 1.8 + 32;
+        System.out.println(temperatureFahrenheit);
     }
+
 }
